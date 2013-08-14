e071770e221bcdeb59c2a385c066f250f5e2c34b
Jeff Buchbinder <jeffreymbuchbinder@gmail.com>
2013-08-14 10:24:48 -0400

    Call signature generation ( http://www.shutterfly.com/documentation/OflyCallSignature.sfly )

--- object.go
+++ object.go
@@ -1,5 +1,9 @@
 package shutterfly
 
+const (
+	APP_ID = "179e2c6d91b95bbc8abf2673c181ee70"
+)
+
 type Shutterfly struct {
 	AuthToken string
 }
--- util.go
+++ util.go
@@ -3,6 +3,8 @@ package shutterfly
 import (
 	"bytes"
 	"encoding/xml"
+	"net/http"
+	"time"
 )
 
 // Wrap xml.Escape() so that it can be simply used with strings
@@ -10,4 +12,12 @@ func XmlEncode(s string) string {
 	w := bytes.NewBuffer([]byte{})
 	xml.Escape(w, []byte(s))
 	return w.String()
+}
+
+func AuthHeaders(req *http.Request, urlParams string) {
+	now := time.Now()
+
+	req.Header.Set("oflyAppId", APP_ID)
+	req.Header.Set("oflyHashMeth", "SHA1")
+	req.Header.Set("oflyTimestamp", now.Format("2006-01-02T15:04:05.000-0700"))
 }
